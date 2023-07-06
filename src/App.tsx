0de3564602e89f85053c24afbc16d80bad70ae47
Axent13 <axent13@yandex.ru>
2023-07-06 09:28:44 +0700

    build: Добавляет первоначальную настройку проекта

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography } from "antd";
+
+const { Title } = Typography;
+
+const App: React.FC = () => {
+  return <Title>Hello World</Title>;
+};
+
+export default App;
