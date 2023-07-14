1e8e7abeac805dc84701b1fe04072f42c14d48cb
Axent13 <axent13@yandex.ru>
2023-07-14 14:19:08 +0700

    feat: Добавляет защищенный роут для старницы с квизом

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import QuizPage from 'pages/QuizPage/QuizPage';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from 'pages/MainPage/MainPage';
+import localStorageService from 'services/localStorage.service';
 
 const App = () => {
+  let isLoggedIn = false;
+  if (localStorageService.getUserEmail()) {
+    isLoggedIn = true;
+  }
+
   return (
     <Routes>
       <Route index element={<MainPage />} />
-      <Route path='quiz' element={<QuizPage />} />
+      <Route
+        path='quiz'
+        element={isLoggedIn ? <QuizPage /> : <Navigate to='/' />}
+      />
       <Route path='*' element={<Navigate to='/' />} />
     </Routes>
   );
