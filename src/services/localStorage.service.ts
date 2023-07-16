69f136385c8453af1093f2a7338ce6ad67c357b1
Axent13 <axent13@yandex.ru>
2023-07-16 11:37:09 +0700

    refactor: Переписывает сервисы на TypeScript

--- src/services/localStorage.service.ts
+++ src/services/localStorage.service.ts
@@ -1,21 +1,24 @@
+import { localStorageItems } from 'types/localStorage';
+
 const TOKEN_KEY = 'jwt-token';
 const REFRESH_KEY = 'jwt-refresh-token';
 const EXPIRES_KEY = 'jwt-expires';
 const USERID_KEY = 'user-local-id';
 const USER_EMAIL = 'user-email';
+const EXPIRES_TIME = 3600;
 
 export function setTokens({
   refreshToken,
   idToken,
   localId,
   email,
-  expiresIn = 3600,
-}) {
-  const expiresDate = new Date().getTime() + expiresIn * 1000;
+}: localStorageItems) {
+  const expiresDate = new Date().getTime() + EXPIRES_TIME * 1000;
+  console.log('expiresDate', expiresDate);
   localStorage.setItem(USERID_KEY, localId);
   localStorage.setItem(TOKEN_KEY, idToken);
   localStorage.setItem(REFRESH_KEY, refreshToken);
-  localStorage.setItem(EXPIRES_KEY, expiresDate);
+  localStorage.setItem(EXPIRES_KEY, expiresDate.toString());
   localStorage.setItem(USER_EMAIL, email);
 }
 
