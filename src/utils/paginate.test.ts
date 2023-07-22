06d73d01bd07d8d803a4a0eea343fa0e5414dea6
Axent13 <axent13@yandex.ru>
2023-07-22 09:34:55 +0700

    feat: Добавляет тесты для функции paginate

--- src/utils/paginate.test.ts
+++ src/utils/paginate.test.ts
@@ -7,4 +7,41 @@ describe('Функция paginate:', () => {
     const result = paginate(testItems, 1, 1);
     expect(result).toEqual(excpectedResult);
   });
+
+  test('Возвращает массив из одного элемента, если он был передан один', () => {
+    const testItems: Array<object> = [{ text: 'test' }];
+    const excpectedResult: Array<object> = [{ text: 'test' }];
+    const result = paginate(testItems, 1, 1);
+    expect(result).toEqual(excpectedResult);
+  });
+
+  test('Возвращает 2 первых элемента для первой страницы', () => {
+    const testItems: Array<object> = [
+      { text: 'test1' },
+      { text: 'test2' },
+      { text: 'test3' },
+      { text: 'test4' },
+    ];
+    const excpectedResult: Array<object> = [
+      { text: 'test1' },
+      { text: 'test2' },
+    ];
+    const result = paginate(testItems, 1, 2);
+    expect(result).toEqual(excpectedResult);
+  });
+
+  test('Возвращает 3 и 4 элементы для второй страницы', () => {
+    const testItems: Array<object> = [
+      { text: 'test1' },
+      { text: 'test2' },
+      { text: 'test3' },
+      { text: 'test4' },
+    ];
+    const excpectedResult: Array<object> = [
+      { text: 'test3' },
+      { text: 'test4' },
+    ];
+    const result = paginate(testItems, 2, 2);
+    expect(result).toEqual(excpectedResult);
+  });
 });
