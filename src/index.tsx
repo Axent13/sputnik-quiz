29000b76b8457481404b644884612dd07daae4e1
Axent13 <axent13@yandex.ru>
2023-07-22 12:24:09 +0700

    fix: Исправляет роутинг на Github Pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import App from 'app/App';
 import { Provider } from 'react-redux';
 import { store } from 'shared/store';
 import 'app/styles/index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root') as Element);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
